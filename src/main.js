950a2202c49afae6a74be618afc01bdea98f0ba3
b30wulffz <shlokpandey123@gmail.com>
2021-04-13 01:36:37 +0530

    Tested importing a blender model and setting up lights

--- src/main.js
+++ src/main.js
@@ -1,41 +1,122 @@
 import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
+import { OrbitControls } from "https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/loaders/DRACOLoader.js";
 
 const scene = new THREE.Scene();
+// scene.background = new THREE.Color(0xff0000);
 const camera = new THREE.PerspectiveCamera(
-  75,
+  90,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-// const width = window.innerWidth,
-//   height = window.innerHeight;
+
+// const camera = new THREE.PerspectiveCamera(
+//   45,
+//   window.innerWidth / window.innerHeight,
+//   1,
+//   1000
+// // );
+// const width = window.innerWidth;
+// const height = window.innerHeight;
 // const camera = new THREE.OrthographicCamera(
 //   width / -2,
 //   width / 2,
 //   height / 2,
 //   height / -2,
 //   1,
-//   1000
+//   10
 // );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const color = 0xffffff;
+const intensity = 3;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
+
+// const skyColor = 0xb1e1ff; // light blue
+// const groundColor = 0xb97a20; // brownish orange
+// const intensity = 3;
+// const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+// scene.add(light);
+
+// var light = new THREE.PointLight(0xffffcc, 20, 200);
+// light.position.set(4, 30, -20);
+// scene.add(light);
+
+// var light2 = new THREE.AmbientLight(0x20202a, 20, 100);
+// light2.position.set(30, -10, 30);
+// scene.add(light2);
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// let cube = new THREE.Mesh(geometry, material);
+// console.log(cube);
+// scene.add(cube);
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("src/libs/draco/gltf/");
+
+var loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+// let cube;
+// loader.load("src/assets/testcube.glb", function (gltf) {
+//   // var scale = 5.6;
+//   // bus.body = gltf.scene.children[0];
+//   // console.log(gltf);
+//   // // bus.body.name = “body”;
+//   // bus.body.rotation.set(0, -1.5708, 0);
+//   // bus.body.scale.set(scale, scale, scale);
+//   // bus.body.position.set(0, 3.6, 0);
+//   // bus.body.castShadow = true;
+//   // bus.frame.add(bus.body);
+//   // console.log(gltf.scene);
+//   // cube = gltf.scene.children;
+//   const obj = gltf.scene;
+//   console.log(obj.position);
+//   scene.add(gltf.scene.children);
+// });
+
+// loader.load(
+//   "src/assets/testcube.glb",
+//   function (gltf) {
+//     const model = gltf.scene;
+//     model.position.set(1, 1, 0);
+//     model.scale.set(0.01, 0.01, 0.01);
+//     model.traverse(function (child) {
+//       if (child.isMesh) child.material.envMap = envMap;
+//     });
+
+//     scene.add(model);
+
+//     mixer = new THREE.AnimationMixer(model);
+//     mixer.clipAction(gltf.animations[0]).play();
+
+//     animate();
+//   },
+//   undefined,
+//   function (e) {
+//     console.error(e);
+//   }
+// );
+
+const [cubeData] = await Promise.all([
+  loader.loadAsync("src/assets/testcube.glb"),
+]);
+// extracting mesh
+let cube = cubeData.scene.children[2];
 
 scene.add(cube);
 
-// camera.position.z = 5;
-camera.position.z = 6;
+camera.position.z = 5;
 
 // movement - please calibrate these values
 var xSpeed = 0.5;
 var ySpeed = 0.5;
-
-// document.body.addEventListener("keydown", onDocumentKeyDown);
 
 cube.newPosition = new THREE.Vector3(
   cube.position.x,
@@ -74,18 +155,6 @@ const onKeyDown = (event) => {
       }
       break;
   }
-
-  // if (keyCode == 87) {
-  //   cube.position.y += ySpeed;
-  // } else if (keyCode == 83) {
-  //   cube.position.y -= ySpeed;
-  // } else if (keyCode == 65) {
-  //   cube.position.x -= xSpeed;
-  // } else if (keyCode == 68) {
-  //   cube.position.x += xSpeed;
-  // } else if (keyCode == 32) {
-  //   cube.position.set(0, 0, 0);
-  // }
 };
 
 document.addEventListener("keydown", onKeyDown);
